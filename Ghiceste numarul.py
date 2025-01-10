16e8257a4a8be8f56a9e84b34310dd15ecf6a00d
Coconutu <sebastian.daniel.adrian@gmail.com>
2025-01-10 21:10:32 +0200

    ghiceste numarul

---
+++ Ghiceste numarul.py
@@ -0,0 +1,13 @@
+import random
+numar=random.randint(1,20)
+ghicit=False
+while ghicit==False:
+    introdus=int(input('Scrie numarul:'))
+    if introdus<numar:
+        print("Prea mic. Mai incearca!")
+    elif introdus>numar:
+        print("Prea mare. Mai incearca!")
+    else:
+        print("Felicitari, ai chicit! Era numarul",numar)
+        break
+
