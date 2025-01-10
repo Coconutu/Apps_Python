a3b4cb6441f430e4a87bc4241b161cf1e8c213b5
Coconutu <sebastian.daniel.adrian@gmail.com>
2025-01-10 22:31:49 +0200

    mini proiect 2 - meniu si operatii matematice

---
+++ seturi_multimi.py
@@ -0,0 +1,23 @@
+# set1 = {1,2,3,4,5,6,7,8,9}
+# #afișăm setul creat
+# print("Mulțimea este: ",set1)
+# #afișăm numărul de elemente
+# print("Număr de elemente:",len(set1))
+# #folosim unii operatori
+# print(1 in set1)
+# print(5 not in set1)
+# #un alt set cu elemente de tipuri diferite
+# set2 = {12.5,8.7,4,"text",("un","tuplu")}
+# print("Altă mulțime:",set2
+#
+set1 = {'A','B','C'}
+print('Setul inițial:',set1)
+set1.add('D') #doar un element
+set1.update('E','F','G') #mai multe deodată
+print('După adăugări:',set1)
+#ștergem elementul 'E'
+set1.discard('E')
+#ștergem elementul 'B'
+set1.remove('B')
+#afișăm lista modificată
+print('După ștergeri:',set1)
