9b6ce2ba03c7135c10c3c56dd0b426874b135ca3
Coconutu <sebastian.daniel.adrian@gmail.com>
2025-01-10 21:19:48 +0200

    notarea incepe de la 1 si pentru fiecare raspuns corect, se adauga 0.9 puncte

--- Operatii inmultire.py
+++ Operatii inmultire.py
@@ -1,7 +1,7 @@
 import random
 from random import randint
 rez=0
-nota=0
+nota=1
 for x in range(10):
     a=int(random.randint(4,9))
     b=int(random.randint(4,9))
@@ -9,7 +9,7 @@ for x in range(10):
     rasp=int(input(str(a)+"*"+str(b)+"="))
     if rez==rasp:
         print("Corect")
-        nota=nota+1
+        nota=nota+0.9
     else:
         print("Incorect")
-print("Nota este ",nota,"/10")
+print("Nota este ",round(nota),"/10")
