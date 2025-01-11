bf710f3b277fb2235a204791f04f48b88d09c43e
Coconutu <sebastian.daniel.adrian@gmail.com>
2025-01-11 08:11:37 +0200

    problema 2-seturi

---
+++ Problema 2.py
@@ -0,0 +1,16 @@
+'''Seturi
+a) Creați două seturi cu numere întregi și găsiți intersecția dintre ele (elementele comune).
+b) Verificați dacă un anumit element există într-un set de caractere.
+c) Eliminați elementul duplicat dintr-un set.
+d) Uniți două seturi și afișați setul rezultat.
+'''
+m1={1,2,3,4,5,6}
+m2={4,5,6,7,8,9}
+print(m1.intersection(m2))
+print(1 in m1)
+print("Setul original m1")
+print(m1)
+print(m1.union(m2))
+
+
+
