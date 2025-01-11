662e364959e4ca64620a451e12aa10d36257b744
Coconutu <sebastian.daniel.adrian@gmail.com>
2025-01-11 08:20:53 +0200

    problema 3- dictionare

---
+++ Problema 3.py
@@ -0,0 +1,14 @@
+'''Dicționare
+a) Creați un dicționar cu denumirile și notele a 5 studenți și afișați-l.
+b) Afișați numărul de elemente dintr-un dicționar.
+c) Afișați cheia cu cea mai mare valoare dintr-un dicționar de numere întregi.
+d) Creați un dicționar cu numele și vârstele a 4 prieteni. Apoi, adăugați o
+pereche cheie-valoare nouă pentru un alt prieten.'''
+dict={"Student1":10,"Student2":9,"Student3":9,"Student4":6,"Student5":7}
+print(dict)
+print(len(dict))
+dicti={2:5,5:6,7:8,9:100}
+print(max(dicti))
+dict3={'Sebastian':41,'Cristi':39,'Sorin':69,'Zeni':73}
+dict3['Pavel']=73
+print(dict3)
