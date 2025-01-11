a3e2fe636c0598e113342b4fe2a8d00daf7396f5
Coconutu <sebastian.daniel.adrian@gmail.com>
2025-01-12 00:14:16 +0200

    Miniproiect-Grup de prieteni

--- Miniproiect- Grup de prieteni.py
+++ Miniproiect- Grup de prieteni.py
@@ -14,8 +14,37 @@
 int1=('12.01.2025','Oradea','Vasile Ionas','Grigorescu Pavel','Rebeca Antonia','Selejan Aurel')
 int2=('11.06.2025','Arad','Saul Petrescu','Grigorescu Pavel','Rebeca Antonia','Stoian Vasile')
 int3=('13.10.2025','Bucuresti','Vasile Ionas','Grigorescu George','Rebeca Vaszelena','Selejan Petre')
-int4=('21.11.2025','Bucuresti','Vasile Ionas','Patrascu George','Rebeca Teodora','Selejan Vasile')
-int5=('24.12.2025','Bucuresti','Vasile Ioana','Calinescu George','Selejan Vasile')
+int4=('21.11.2025','Iasi','Vasile Ionas','Patrascu George','Rebeca Teodora','Selejan Vasile')
+int5=('24.12.2025','Galati','Vasile Ionas','Calinescu George','Selejan Vasile')
 
-locatii={int1[1],int2[1],int3[1]}
-print(locatii)
+locatii={int1[1],int2[1],int3[1],int4[1],int5[1]}
+print("Locatii unice unde s-au realizat intalnirile:")
+print('------------------------------')
+
+for x in locatii:
+    print(x)
+print('------------------------------')
+int1p=int1[2:]
+int2p=int2[2:]
+int3p=int3[2:]
+int4p=int4[2:]
+int5p=int5[2:]
+total_prieteni=set(int1p+int2p+int3p+int4p+int5p)
+print("Numarul de prieteni la intalniri:",len(total_prieteni))
+print("Numele prietenilor:")
+for x in total_prieteni:
+    print(x)
+print('------------------------------')
+prieten=input("Introduceti numele prietenului pentru a afla intalnirile unde a participat:")
+if prieten in int1:
+    print(int1[1])
+if prieten in int2:
+    print(int2[1])
+if prieten in int3:
+    print(int3[1])
+if prieten in int4:
+    print(int4[1])
+if prieten in int5:
+    print(int5[1])
+
+
