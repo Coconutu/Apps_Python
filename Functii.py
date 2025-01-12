d8fe890264770cc978d4cbf13bb9f2a3e475d4b3
Coconutu <sebastian.daniel.adrian@gmail.com>
2025-01-12 14:47:21 +0200

    adaugare functie

--- Functii.py
+++ Functii.py
@@ -3,5 +3,14 @@ def subp(n):
     for i in range(n):
         s=s+1/(i+1)
     return s
-n=int(input("Introduceti n"))
-print(subp(n))
+# n=int(input("Introduceti n"))
+# print(subp(n))
+
+def creare_lista(dim):
+    lista_locala=[]
+    for i in range(dim):
+        element=input("Element "+str(i)+":")
+        lista_locala.append(element)
+    return lista_locala
+n=int(input("De cate elemente sa fie lista"))
+print(creare_lista(n))
