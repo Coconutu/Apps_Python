7b937c512faec090ead8313f1c22c459aeab8afd
Coconutu <sebastian.daniel.adrian@gmail.com>
2025-01-16 23:41:09 +0200

    Adaugare stergere fisier

--- Lucrul cu Fisiere/Exercitiul 4.py
+++ Lucrul cu Fisiere/Exercitiul 4.py
@@ -1,3 +1,6 @@
+import os
+from os import remove
+
 lista=[]
 with open("date.out","r") as f:
     lista=f.read()
@@ -14,4 +17,14 @@ with open("date2.out",'w') as g:
 with open("date2.out","r") as f:
     lista1=f.read()
 print(lista1)
+
+with open("date3.out",'w') as g:
+    for i in LISTA:
+        g.write(i)
+
+if os.path.exists("date3.out"):
+    remove("date3.out")
+    print("Fisierul date2.out a fost sters!")
+else:
+    print("Fisierul nu exista!")
 
