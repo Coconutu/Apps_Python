4a974cf06e2be432d19c7566e6e5b779d8a2d2c1
Coconutu <sebastian.daniel.adrian@gmail.com>
2025-01-19 22:11:53 +0200

    Vectori

--- Vectori/citire_fisier_vectori.py
+++ Vectori/citire_fisier_vectori.py
@@ -1,15 +1,13 @@
+from os import write
+
 import utile_vectori as uv
-with open("date.in","r") as f:
-    n=int(f.readline())
-    linie_sir=f.readline().replace("\n"," ")
-    linie_lista=linie_sir.split()
-    vector=[int(el) for el in linie_lista]
-    print('Valoarea lui n=',n)
-    print("Vectorul este")
-    uv.afisare(vector)
-lista="1 2 3 4 5 6"
-print(lista)
-lista_splituita=lista.split()
-print(lista_splituita)
-vector=[int(ek) for ek in lista_splituita]
+n,vector=uv.citeste_f()
+print("Valoarea lui n",n)
+print("Vectorul este")
 uv.afisare(vector)
+
+def scrie_f(v):
+    with open("date.out","w") as f:
+        for el in v:
+            f.write(str(el)+" ")
+        f,write("\n")
--- Vectori/utile_vectori.py
+++ Vectori/utile_vectori.py
@@ -6,7 +6,27 @@ def citire(n,v,tip):
             v.append(float(input("v["+str(i)+"]=")))
         else:
             v.append(input("v[" + str(i) + "]="))
+
+def citeste_f(tip):
+    with open("date.in","r") as f:
+        n=int(f.readline())
+        linie_sir=f.readline().replace("\n"," ")
+        linie_lista=linie_sir.split()
+        if tip=="int"
+            return[n,[int(el) for el in linie_lista]]
+        elif tip=="float"
+            return[n,[float(el) for el in linie_lista]]
+        else:
+            return [n, linie_lista]]
+
 def afisare(v):
     for el in v:
         print(el,end=" ")
     print()
+
+def scrie_f(v):
+    with open("date.out","w") as f:
+        for el in v:
+            f.write(str(el)+" ")
+        f,write("\n")
+
