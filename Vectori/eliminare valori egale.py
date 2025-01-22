2d95d7815eb5c57e012d1b20dbf449b5341ad179
Coconutu <sebastian.daniel.adrian@gmail.com>
2025-01-22 05:29:53 +0200

    Adaugare metoda noua

--- Vectori/eliminare valori egale.py
+++ Vectori/eliminare valori egale.py
@@ -1,3 +1,4 @@
+#metoda naiva
 import utile_vectori as uv
 #citesc datele din fisierul date.in
 n,v=uv.citeste_f("int")
@@ -7,5 +8,24 @@ for i in v:
     if i not in vd:
         vd.append(i)
 #afisez datele
+uv.afisare(v)
+uv.afisare(vd)
+
+# Varianta 2. Metoda „list comprehension”
+import utile_vectori as uv
+#citesc datele din fisierul date.in
+n,v=uv.citeste_f("int")
+vd=[]
+#pur si simplu
+[vd.append(i) for i in v if i not in vd]
+#afisez datele
+uv.afisare(v)
+uv.afisare(vd)
+
+#Varianta 3. Folosim seturile de date
+import utile_vectori as uv
+#citesc datele din fisierul date.in
+n,v=uv.citeste_f("int")
+vd=list(set(v))
 uv.afisare(v)
 uv.afisare(vd)
