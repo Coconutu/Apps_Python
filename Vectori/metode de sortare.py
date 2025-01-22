65bb95e44eaeaf9595adda977b7cae66aa2caa9f
Coconutu <sebastian.daniel.adrian@gmail.com>
2025-01-22 07:38:37 +0200

    adaugare sortare descrescatoare

--- Vectori/metode de sortare.py
+++ Vectori/metode de sortare.py
@@ -1,4 +1,5 @@
 # Se citesc n numere întregi. Se cere ca acestea sǎ fie scrise în ordine crescǎtoare (descrescǎtoare).
+#Sortarea prin selectarea minimului
 import utile_vectori as uv
 #citesc datele
 n,A=uv.citeste_f("int")
@@ -13,7 +14,27 @@ for i in range(n-1):
     man=A[k]
     A[k]=A[i]
     A[i]=man
+    #sau A[i],A[k]=A[k],A[i], fara a mai fi nevoie de variabila de manevra man
 #afisez vectorul sortat
 print("Sortat")
 uv.afisare(A)
+
+#Sortare descrescatoare
+print("Sortare descrescatoare")
+n,B=uv.citeste_f("int")
+print("Initial");uv.afisare(B)
+for i in range(n-1):
+    max=B[i]
+    k=i
+    for j in range(i+1,n):
+        if B[j]>max:
+            max=B[j]
+            k=j
+    man=B[k]
+    B[k]=B[i]
+    B[i]=man
+print("Dupa sortare descrescatoare")
+uv.afisare(B)
+
+
 
