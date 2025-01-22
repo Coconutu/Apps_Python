06f42531228364acf670d02c66a7784c616e1af2
Coconutu <sebastian.daniel.adrian@gmail.com>
2025-01-22 05:21:32 +0200

    Afisare index numere egale

--- Vectori/elemente distincte.py
+++ Vectori/elemente distincte.py
@@ -5,7 +5,17 @@ n,v=uv.citeste_f("int")
 gasit=0
 for i in range(n-1):
     for j in range(i+1,n):
-        if v[i]==v[j]:gasit=1;break
+        if v[i]==v[j]:
+            gasit=1
+            break
     if gasit:break
-if gasit:print('Numerele nu sunt distincte')
-else: print("Numerele sunt distincte")
+if gasit:
+    print('Numerele nu sunt distincte')
+    print("Pozitiile primelor doua valori egale gasite sunt ", i, j)
+else:
+    print("Numerele sunt distincte")
+
+
+
+'''EXERCIȚIU PROPUS. Modificați algoritmul a.î. să fie afișate pozițiile primelor două 
+valori egale găsite, dacă acestea există.'''
