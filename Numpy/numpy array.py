22fa3dc6606ec9ca217dc989f70858c44f2e48db
Coconutu <sebastian.daniel.adrian@gmail.com>
2025-01-22 08:00:59 +0200

    tablouri in numpy

---
+++ Numpy/numpy array.py
@@ -0,0 +1,11 @@
+import numpy as np
+#declarm un vector
+A=np.array([1,2,3,4,5,6])
+#declaram o matrice
+M=np.array([[1,2,3],[4,5,6],[7,8,9]])
+#le afisam in consola
+print(A)
+print(M)
+print(A[2])
+print(type(A[2]))
+#in numpy, tablourile sunt omogene, nu pot contine mai multe tipuri de date
