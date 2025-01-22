5da770764258fe2062dde00e42471d1c846edd8e
Coconutu <sebastian.daniel.adrian@gmail.com>
2025-01-22 09:00:18 +0200

    problema 1

---
+++ Numpy/problema 1.py
@@ -0,0 +1,23 @@
+'''1. Se citeşte un vector cu n componente, numere naturale. Sǎ se afişeze
+cel mai mare numǎr raţional subunitar în care numǎrǎtorul şi numitorul fac
+parte din mulţimea valorilor citite.
+
+Exemplu: dacǎ am citit valorile 1 2 3 se afişeazǎ 2/3.'''
+#citim n
+n=int(input("Numarul de componente ale vectorului. n="))
+#citim vectorul
+v=[]
+for i in range (n):
+    c=input("v["+str(i)+"]=")
+    v.append(c)
+
+
+
+
+
+
+
+
+
+
+
