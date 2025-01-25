1f68a3b89fc65d7989ee78f3648ae34e82ff9553
Coconutu <sebastian.daniel.adrian@gmail.com>
2025-01-25 18:08:29 +0200

    Problema 7- rezolvata

--- Vectori/Problema 7.py
+++ Vectori/Problema 7.py
@@ -1,11 +1,21 @@
 '''7. Se citeşte un numǎr natural n (citirea se va face într-o variabilǎ de tip int). Sǎ se scrie acest numǎr într-un vector.
 Exemplu. Dacǎ citim 1231, vom avea V[1]=1, V[2]=2, V[3]=3, V[4]=1.'''
-import utile_vectori as uv
+
 n=int(input("Introduceti numarul n :"))
 lungime=len(str(n))
-print(lungime)
-for i in range(lungime):
-    c=n%10 #ultima cifra
+vector=[]
+for i in range(0,lungime):
+    c=int(n%10) #ultima cifra
+    n=n/10
+    vector.append(c)
+vector.reverse()
+print(vector)
+
+
+
+
+
+
 
 
 
