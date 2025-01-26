fd320c41325a51275e0862a3b3609ce29e49048b
Coconutu <sebastian.daniel.adrian@gmail.com>
2025-01-26 15:31:07 +0200

    Problema 9- rezolvata

---
+++ Vectori/Problema 9.py
@@ -0,0 +1,9 @@
+'''9. Sǎ se calculeze 2 la puterea 100'''
+rez=1
+for i in range(0,100):
+    rez=rez*2
+print("2 la puterea 100 este :")
+print(rez)
+#modalitatea a doua
+import math
+print(2**100)
