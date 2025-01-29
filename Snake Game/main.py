df62a411e12b0a6f6a251671f51226179c80bec0
Coconutu <sebastian.daniel.adrian@gmail.com>
2025-01-29 07:57:10 +0200

    Snake game, update class Snake

--- Snake Game/main.py
+++ Snake Game/main.py
@@ -18,7 +18,8 @@ class Snake:
         for i in range(BODY_PARTS):
             self.coordinates.append([0,0])
         for x,y in self.coordinates:
-            square=canvas.create_rectangle(x,y,x+SPACE_SIZE,y+SPACE_SIZE,fill=SNAKE_COLOR,TAG="snake")
+            square=canvas.create_rectangle(x,y,x+SPACE_SIZE,y+SPACE_SIZE,fill=SNAKE_COLOR,tag="snake")
+            self.squares.append(square)
 class Food:
     def __init__(self): #this will construct food object
         x=random.randint(0,int((GAME_WIDTH/SPACE_SIZE))-1)*SPACE_SIZE
