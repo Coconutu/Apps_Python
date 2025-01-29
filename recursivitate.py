ea3866906e31916cb2875ed07871cc4bed9840b9
Coconutu <sebastian.daniel.adrian@gmail.com>
2025-01-29 07:16:48 +0200

    recursivitate

--- recursivitate.py
+++ recursivitate.py
@@ -3,3 +3,17 @@ def recursiv1(n):
         print(n)
         recursiv1(n-1)
 recursiv1(7)
+
+def suma(n):
+    if n>0:
+        return n+suma(n-1)
+    else:
+        return n
+print("Suma este",suma(7))
+
+def produsul(n):
+    if n>0:
+        return n*(n-1)
+    else:
+        return n
+print(produsul(3))
