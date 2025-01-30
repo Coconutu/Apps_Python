6a69e4560cb458facc8952f1ba6fde4c8b5ede4e
Coconutu <sebastian.daniel.adrian@gmail.com>
2025-01-30 07:25:32 +0200

    Calculatorul ghiceste

---
+++ Vectori/calc ghiceste.py
@@ -0,0 +1,33 @@
+'''Scrieţi un subprogram recursiv prin care calculatorul ghiceşte un număr
+natural ascuns de dumneavoastră (numărul este cuprins între 1
+şi 30000). Atunci când calculatorul propune un număr i, se va
+răspunde prin:
+1, dacă numărul este prea mare;
+2, dacă numărul este prea mic;
+0, dacă numărul a fost ghicit.'''
+import random
+a=0
+b=30000
+def genereaza_int(a,b):
+    n=random.randint(a,b)
+    return n
+
+def ghiceste(a,b):
+    print("Apasati 1 daca numarul este prea mare, 2 daca este prea mic si 0 daca este cel la care v-ati gandit!")
+
+    i=genereaza_int(a,b)
+    print("Numarul este ",i," ?")
+    n=int(input())
+    if n==0:
+        ghicit=True
+        print("Felicitari !")
+    elif n==2:
+        a=i
+        ghiceste(a,b)
+    elif n==1:
+        b=i
+        ghiceste(a,b)
+    else:
+        print("Apasati 1 daca numarul este prea mare, 2 daca este prea mic si 0 daca este cel la care v-ati gandit!")
+ghiceste(1,30000)
+
