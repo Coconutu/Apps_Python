a87a7714ae624a691798fc22c145d2927d77dff3
Coconutu <sebastian.daniel.adrian@gmail.com>
2025-02-02 16:09:04 +0200

    adaugare try, except, finally

--- Tratarea eroorilor/Tratarea eroorilor 2.py
+++ Tratarea eroorilor/Tratarea eroorilor 2.py
@@ -1,8 +1,12 @@
-n=int(input("Introduceti varsta (intre 3 si 120)"))
-anul_nasterii=2025-n
-if n<3 or n>120:
-    print("-------------------------------")
-    print("Eroare. Va rugam sa respectati intervalul")
-    print("Reporniti programul")
-else:
-    print("Anul nasterii:",anul_nasterii,".",sep="")
+try:
+    n=int(input("Introduceti varsta (intre 3 si 120)"))
+    anul_nasterii=2025-n
+    if n>=3 and n<=120:
+        print("Anul nasterii:", anul_nasterii, ".", sep="")
+    else:
+        print("Respecta intervalul!")
+except ValueError:
+    print("Ups. Nu ai introdus un numar natural")
+    print("Reporneste programul")
+finally:
+    print("Program terminat ok")
