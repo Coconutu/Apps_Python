aa52296590301461b91936ccea2f0da5bfc2c7c8
Coconutu <sebastian.daniel.adrian@gmail.com>
2025-02-02 15:53:36 +0200

    Clasa Pers

---
+++ Structuri de date/Persoane.py
@@ -0,0 +1,11 @@
+class Pers:
+    pass
+p1=Pers() #un nou obiect, o noua inregistrare
+p1.nume="Mihai"
+p1.varsta=26
+p1.inaltime=1.82
+p2=Pers()
+p2.nume="Vasile"
+p2.varsta=23
+p2.inaltime=1.90
+print(p1)
