cd0766dbd1bb3673b3041758e7948d9e9aa1ebb2
Coconutu <sebastian.daniel.adrian@gmail.com>
2025-02-03 07:40:51 +0200

    tratarea eroorilor

--- aflare suma dif cat rest.py
+++ aflare suma dif cat rest.py
@@ -1,12 +1,18 @@
-n1=int(input("introduceti doua numere naturale nenule "))
-n2=int(input())
-print("suma este ",n1+n2)
-print("diferenta este ",n1-n2)
-print("produsul este ",n1*n2)
-print("catul este ",n1//n2)
-print("restul este ",n1%n2)
-
-
+try:
+    n1=int(input("introduceti doua numere naturale nenule "))
+    n2=int(input())
+    if n2==0:raise ZeroDivisionError
+    print("suma este ",n1+n2)
+    print("diferenta este ",n1-n2)
+    print("produsul este ",n1*n2)
+    print("catul este ",n1//n2)
+    print("restul este ",n1%n2)
+except ValueError:
+    print("Numerele trebuie sa fie naturale")
+except ZeroDivisionError:
+    print("al doilea numar trebuie sa fie diferit de zero")
+finally:
+    print("Program terminat ok")
 
 
 
