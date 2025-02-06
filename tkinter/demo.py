473091b29c76d3468832eb9b6a45ebf4d86647da
Coconutu <sebastian.daniel.adrian@gmail.com>
2025-02-06 07:53:22 +0200

    primula aplicatie tk

---
+++ tkinter/demo.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+#o instanta a ferestrei principale
+aplicatie=tk.Tk()
+
+#setez titlul ferestrei
+aplicatie.title("Prima mea aplicatie")
+
+#setez dimensiunea ferestrei
+aplicatie.geometry("400x300")
+
+#pornesc bucla evenimentului principal
+aplicatie.mainloop()
