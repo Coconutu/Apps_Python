4660b520b0e202bd08c242effa427c9ce99f6739
Coconutu <sebastian.daniel.adrian@gmail.com>
2025-02-06 08:02:40 +0200

    primula aplicatie tk

--- tkinter/demo.py
+++ tkinter/demo.py
@@ -2,9 +2,9 @@ import tkinter as tk
 aplicatie=tk.Tk()#o instanta a ferestrei principale
 aplicatie.title("Prima mea aplicatie")#setez titlul ferestrei
 aplicatie.geometry("400x300")#setez dimensiunea ferestrei
-eticheta=tk.Label(aplicatie,text="Bine ati venit!")
+eticheta=tk.Label(aplicatie,text="Bine ati venit!") #eticheta este variabila ce retine referinta la obiectul Label
 eticheta.pack()
 buton=tk.Button(aplicatie,text="ok",command=lambda:print("Buton apasat"))
-buton.pack()
+buton.pack()#pentru a afisa butonul, fara aceasta instructiune, butonu ramane creat doar in memorie, nu ar fi afisat
 
 aplicatie.mainloop()#pornesc bucla evenimentului principal
