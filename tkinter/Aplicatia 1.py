f3dd21e4a1d257982b3963e2ea8d61ff0de55a84
Coconutu <sebastian.daniel.adrian@gmail.com>
2025-02-09 20:35:43 +0200

    Aplicatie 1 - date de contact

--- tkinter/Aplicatia 1.py
+++ tkinter/Aplicatia 1.py
@@ -1,3 +1,22 @@
 '''Cerință. Se cere să se realizeze un program care solicită utilizatorului să
 introducă numele, prenumele și o adresă de email, apoi să afișeze un mesaj
 drăguț de bun venit, care să confirme înregistrarea datelor în program.'''
+import tkinter as tk
+from tkinter import messagebox as mb
+from tkinter.simpledialog import askstring
+
+date_utilizator={"Nume":"","Prenume":"","Email":""}
+def solicita_informatie(info):
+    rezultat=askstring("Input",f"Introduceti {info}")
+    if rezultat:
+        date_utilizator[info]=rezultat
+        if all(date_utilizator.values()):
+            mesaj=f"Bun venit,{date_utilizator['Nume']},{date_utilizator['Prenume']}!\n"
+            f"Emailul tau,{date_utilizator['Email']},a fost inregistrat cu succes."
+            mb.showinfo("Inregistrare completa",mesaj)
+aplicatie=tk.Tk()
+aplicatie.title("Inregistrare")
+buton_n=tk.Button(aplicatie,"Introduceti numele",command=lambda:solicita_informatie("Nume"));buton_n.pack()
+buton_p=tk.Button(aplicatie,"Introduceti prenumele",command=lambda:solicita_informatie("Prenume"));buton_p.pack()
+buton_e=tk.Button(aplicatie,"Introduceti emailul",command=lambda:solicita_informatie("Email"));buton_e.pack()
+aplicatie.mainloop()
