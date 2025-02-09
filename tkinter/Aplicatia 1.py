511775fce9c00fc595d87b1f4e7faf724ed46e36
Coconutu <sebastian.daniel.adrian@gmail.com>
2025-02-09 20:00:53 +0200

    Aplicatie 1 - date de contact

---
+++ tkinter/Aplicatia 1.py
@@ -0,0 +1,3 @@
+'''Cerință. Se cere să se realizeze un program care solicită utilizatorului să
+introducă numele, prenumele și o adresă de email, apoi să afișeze un mesaj
+drăguț de bun venit, care să confirme înregistrarea datelor în program.'''
