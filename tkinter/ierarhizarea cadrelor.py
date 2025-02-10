f337f85af39c9c6814f3f72f82cfa5bff4914379
Coconutu <sebastian.daniel.adrian@gmail.com>
2025-02-10 16:17:22 +0200

    cadre

---
+++ tkinter/ierarhizarea cadrelor.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+from tkinter import ttk
+
+from winnt import IMAGE_DIRECTORY_ENTRY_IAT
+
+aplicatie = tk.Tk()
+aplicatie.title('Cadre imbricate')
+# Nivelul superior
+cadru_exterior = ttk.Frame(aplicatie, padding='10', relief='sunken', borderwidth=5)
+cadru_exterior.pack(fill='both', expand=True)
+#adaugarea unui cadru in interiorul celui superior
+cadru_interior=
+'''ujoc maya, lacatos medana, puscoci alesia motivare absente'''
