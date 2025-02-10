75d1caae509747559ccdce4d6aee6beb00cfe574
Coconutu <sebastian.daniel.adrian@gmail.com>
2025-02-10 09:10:02 +0200

    Dialoguri tkinter

--- tkinter/Dialog.py
+++ tkinter/Dialog.py
@@ -7,11 +7,17 @@ def intrebare():
     else:
         print("Gresit")
 
-
+def ok_anulare():
+    rezult=ms.askokcancel("Intrebare","Doriti sa continuam")
+    if rezult=="ok":
+        print("Continuam")
+    else:
+        print("anulam")
 aplicatie=tk.Tk()
 aplicatie.title("Dialog tkinter")
 aplicatie.geometry("400x300")
 
 buton1=tk.Button(aplicatie,text="Dialog 1",command=intrebare);buton1.pack()
+buton2=tk.Button(aplicatie,text="Dialog 2",command=ok_anulare);buton2.pack()
 
 aplicatie.mainloop()
