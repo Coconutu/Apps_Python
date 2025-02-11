f6edbe4daa81cca49a03a5926069b9e5cb01292f
Coconutu <sebastian.daniel.adrian@gmail.com>
2025-02-11 22:25:57 +0200

    aplicatie Maria

--- tkinter/Aplicatie_Maria.py
+++ tkinter/Aplicatie_Maria.py
@@ -6,20 +6,22 @@ from docutils.nodes import entry
 
 
 def genereaza_numere():
-    a = random.randint(1, 10)
-    b = random.randint(1, 9)
-
-    return a,b
+    a = random.randint(1, 5)
+    b = random.randint(1, 3)
+    c=a*b
+    return c,a,b
 
 def verifica_raspuns(c,a,b):
-    if c==a*b:
+    if c==a/b:
         ms.showinfo("Info","Raspuns corect!")
-        a,b=genereaza_numere()
+
     else:
         ms.showinfo("Info","Raspuns incorect!")
 
+    ms.showinfo("Info", "a : b=: " + str(c) + "a= " + str(a) + "b= " + str(b))
+    c, a, b = genereaza_numere()
 
-a,b=genereaza_numere()
+c,a,b=genereaza_numere()
 aplicatie = tk.Tk()
 aplicatie.geometry('300x200')
 aplicatie.title("Calcule Maria")
@@ -30,5 +32,6 @@ eticheta2.pack(padx=10,pady=10)
 casuta_text = tk.Entry(aplicatie)
 casuta_text.pack(padx=10,pady=10)
 buton1=tk.Button(aplicatie,text="Trimite rezultat",command=lambda:verifica_raspuns(int(casuta_text.get()),a,b))
+
 buton1.pack()
 aplicatie.mainloop()
