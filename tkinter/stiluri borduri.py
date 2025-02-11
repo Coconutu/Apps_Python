6c9ca039697c5b02065d39e81d803fba8143dde3
Coconutu <sebastian.daniel.adrian@gmail.com>
2025-02-11 07:01:59 +0200

    exemple de parametri

--- tkinter/stiluri borduri.py
+++ tkinter/stiluri borduri.py
@@ -1,9 +1,10 @@
 import tkinter as tk
 from tkinter import ttk
 aplicatie=tk.Tk()
-aplicatie.title("Exemple de parametri")
-cadru=tk.Frame(aplicatie,borderwidth=5,relief='ridge')
+aplicatie.geometry('300x200')
+aplicatie.title("Exemple de parametri.Cursor diferit")
+cadru=tk.Frame(aplicatie,width=20,height=100,bg='lightblue',cursor="hand2",relief='ridge')
 cadru.pack()
 eticheta=ttk.Label(cadru,text="Eticheta din cadru")
-eticheta.pack(pady=5)
+eticheta.pack(pady=50,padx=50)
 aplicatie.mainloop()
