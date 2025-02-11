06090ee4009c5d537c42d155933a6e56755a6b0b
Coconutu <sebastian.daniel.adrian@gmail.com>
2025-02-11 20:52:44 +0200

    aplicatie practica 3

--- tkinter/aplicatia practica 3.py
+++ tkinter/aplicatia practica 3.py
@@ -1,1 +1,12 @@
 import tkinter as tk
+aplicatie=tk.Tk()
+#un camp de text pe linia 0, coloana 0
+entry=tk.Entry(aplicatie)
+entry.grid(row=0,column=0,columnspan=2,padx=10,pady=10)
+#un buton pe linia 1, coloana 0
+button1=tk.Button(aplicatie,text="Buton 1")
+button1.grid(row=1,column=0,padx=10,pady=10)
+#alt buton pe linia 1, coloana 1
+button2=tk.Button(aplicatie,text="Buton 2")
+button2.grid(row=1,column=1,padx=10,pady=10)
+aplicatie.mainloop()
