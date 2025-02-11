4091620a8a3c5c3689115b7e1712b096c882249d
Coconutu <sebastian.daniel.adrian@gmail.com>
2025-02-11 22:06:45 +0200

    aplicatie Maria

--- tkinter/Aplicatie_Maria.py
+++ tkinter/Aplicatie_Maria.py
@@ -1,30 +1,36 @@
 import random
 import tkinter as tk
-rezultat=[1,1,1]
+from tkinter import messagebox as ms
+
+from docutils.nodes import entry
+
 
 def genereaza_numere():
-    a = random.randint(10, 30)
+    global a
+    global b
+    a = random.randint(1, 10)
     b = random.randint(1, 9)
+
     return a,b
 
 def verifica_raspuns(c,a,b):
-    c=int(casuta_text.get())
-    if c=a*b:
-        return "Raspuns corect"
+    if c==a*b:
+        ms.showinfo("Info","Raspuns corect!")
+        a,b=genereaza_numere()
+    else:
+        ms.showinfo("Info","Raspuns incorect!")
 
 
-
-
-rezultat = genereaza_impartire()
+a,b=genereaza_numere()
 aplicatie = tk.Tk()
 aplicatie.geometry('300x200')
 aplicatie.title("Calcule Maria")
 eticheta1 = tk.Label(aplicatie, text="Efectueaza împărțirile :")
 eticheta1.pack(padx=10,pady=10)
-eticheta2 = tk.Label(aplicatie, text=str(rezultat[2]) + " : " + str(rezultat[1]) + " = ")
+eticheta2 = tk.Label(aplicatie, text=str(a*b) + " : " + str(b) + " = ")
 eticheta2.pack(padx=10,pady=10)
 casuta_text = tk.Entry(aplicatie)
 casuta_text.pack(padx=10,pady=10)
-buton1=tk.Button(aplicatie,text="Trimite rezultat",command=trimite_rezultat)
-
+buton1=tk.Button(aplicatie,text="Trimite rezultat",command=lambda:verifica_raspuns(int(casuta_text.get()),a,b))
+buton1.pack()
 aplicatie.mainloop()
