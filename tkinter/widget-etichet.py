abf95633569042a79c4603ea1c3d023e98cd5742
Coconutu <sebastian.daniel.adrian@gmail.com>
2025-02-13 07:45:26 +0200

    etichete

---
+++ tkinter/widget-etichet.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+root=tk.Tk()
+root.geometry("500x200")
+#eticheta cu text simplu
+label1=tk.Label(root,text='Eticheta simplă')
+label1.pack()
+#eticheta cu text ingrosat si culoare de fundal
+label2=tk.Label(root,text='Eticheta cu text ingrosat si culoare de fundal',font=('Arial',12,'bold'),bg='yellow')
+label2.pack()
+#eticheta cu text italic si culoare a textului
+label3=tk.Label(root,text='Eticheta cu text italic si culoare a textului',font=('Helvetica',12,'italic'),fg='red')
+label3.pack()
+root.mainloop()
