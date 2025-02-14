92c462e1db7a0cee1ecf9db58b8f7a4981e32465
Coconutu <sebastian.daniel.adrian@gmail.com>
2025-02-14 20:31:05 +0200

    update application geometry

--- tkinter/Calcule_Maria_Android.py
+++ tkinter/Calcule_Maria_Android.py
@@ -1,6 +1,7 @@
 import random
 import tkinter as tk
 from tkinter import messagebox as ms
+
 font_etichete = ("Arial", 16)
 font_butoane = ("Arial", 13)
 corecte = 0
@@ -9,7 +10,7 @@ MIN_A = 8
 MAX_A = 20
 MIN_B = 2
 MAX_B = 9
-stop=10
+stop = 10
 a = random.randint(MIN_A, MAX_A)
 b = random.randint(MIN_B, MAX_B)
 c = a * b
@@ -17,21 +18,31 @@ aplicatie = tk.Tk()
 aplicatie.resizable(False, False)
 # aplicatie.geometry('350x200')
 aplicatie.title("Calcule Maria")
-def inchide_aplicatie():
-	aplicatie.destroy()
-buton_inchide=tk.Button(aplicatie,text="X",font=("Arial",15,"bold"),command=inchide_aplicatie,relief="raised",borderwidth=10)
-buton_inchide.pack(side="top",anchor="ne")
-eticheta1 = tk.Label(aplicatie, text="Efectueaza împărțirile :", font=font_etichete)
-eticheta1.pack(pady=50)
-eticheta2 = tk.Label(aplicatie, text=str(a * b) + " : " + str(b) + " = ", font=font_etichete)
-eticheta2.pack()
-casuta_text = tk.Entry(aplicatie, font=font_etichete)
-casuta_text.pack(pady=50)
-eticheta_corecte = tk.Label(aplicatie, text="Corecte : 0", font=font_etichete)
-eticheta_corecte.pack()
-eticheta_incorecte = tk.Label(aplicatie, text="Greșite : 0", font=font_etichete)
-eticheta_incorecte.pack(pady=50)
 
+
+def inchide_aplicatie():
+    aplicatie.destroy()
+
+
+buton_inchide = tk.Button(aplicatie, text="X", font=("Arial", 15, "bold"), command=inchide_aplicatie, relief="raised",
+                          borderwidth=10)
+buton_inchide.grid(row=0,column=10)
+eticheta1 = tk.Label(aplicatie, text="Efectueaza împărțirile :", font=font_etichete)
+eticheta1.grid(row=0,column=0)
+eticheta2 = tk.Label(aplicatie, text=str(a * b) + " : " + str(b) + " = ", font=font_etichete)
+eticheta2.grid(row=2,column=0)
+casuta_text = tk.Entry(aplicatie, font=font_etichete)
+casuta_text.grid(row=2,column=0)
+eticheta_corecte = tk.Label(aplicatie, text="Corecte : 0", font=font_etichete)
+eticheta_corecte.grid(row=2,column=0)
+eticheta_incorecte = tk.Label(aplicatie, text="Greșite : 0", font=font_etichete)
+eticheta_incorecte.grid(row=3, column=0)
+for x in range(0,5):
+    buton_nr=tk.Button(aplicatie,text=str(x))
+    buton_nr.grid(row=4, column=2)
+for x in range(5,9):
+    buton_nr=tk.Button(aplicatie,text=str(x))
+    buton_nr.grid(row=5,column=0)
 
 def genereaza_numere():
     global a, b, c
@@ -43,16 +54,16 @@ def genereaza_numere():
 
 
 def verifica_raspuns():
-    global count, corecte, gresite,stop
+    global count, corecte, gresite, stop
     try:
 
         if int(casuta_text.get()) == a:
             corecte += 1
-            if corecte==stop:
-            	ms.showinfo("Felicitari","Ai raspuns corect la "+str(stop)+" \n calcule! Ai gresit "+str(gresite)+" calcule!")
-            	inchide_aplicatie()
-            
-            	
+            if corecte == stop:
+                ms.showinfo("Felicitari", "Ai raspuns corect la " + str(stop) + " \n calcule! Ai gresit " + str(
+                    gresite) + " calcule!")
+                inchide_aplicatie()
+
             eticheta_corecte.config(text="Corecte : " + str(corecte))
             genereaza_numere()
             casuta_text.delete(0, tk.END)
@@ -66,6 +77,7 @@ def verifica_raspuns():
         eticheta_corecte.config(text=" Introduceti rezultatul", font=font_etichete)
 
 
-buton1 = tk.Button(aplicatie, text="Trimite rezultat", font=font_butoane, command=verifica_raspuns,width=50,height=3,bg="lightblue",relief="raised",borderwidth=20)
-buton1.pack(pady=50)
+buton1 = tk.Button(aplicatie, text="Trimite rezultat", font=font_butoane, command=verifica_raspuns, width=50, height=3,
+                   bg="lightblue", relief="raised", borderwidth=20)
+buton1.grid(row=6,column=0)
 aplicatie.mainloop()
