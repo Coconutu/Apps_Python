f3160ae2794e49988ee792763a2de9dd9c2df3a2
Coconutu <sebastian.daniel.adrian@gmail.com>
2025-02-14 07:16:28 +0200

    using PIL, compound

--- tkinter/imagini_pillow.py
+++ tkinter/imagini_pillow.py
@@ -3,11 +3,11 @@ from PIL import Image,ImageTk
 root=tk.Tk()
 imagine_originala=Image.open('poster_interstelar.jpg')
 imagine=ImageTk.PhotoImage(imagine_originala)
-eticheta1 = tk.Label(root, text="Filmul meu preferat",
-font="Calibri 12 bold")
-eticheta1.pack()
 cadru_cu_imagine=tk.Frame(root,padx=20,pady=20)
 cadru_cu_imagine.pack()
-etichete2=tk.Label(cadru_cu_imagine,image=imagine)
-etichete2.pack()
+
+eticheta1 = tk.Label(cadru_cu_imagine, text="Filmul meu preferat",image=imagine,compound='top',font='Calibri 12 bold')
+
+eticheta1.pack()
+
 root.mainloop()
