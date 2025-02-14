61f9f54a5404bfa39d05d4fd89e0b50027ce413d
Coconutu <sebastian.daniel.adrian@gmail.com>
2025-02-14 21:18:26 +0200

    update application geometry

--- tkinter/Calcule_Maria_Android.py
+++ tkinter/Calcule_Maria_Android.py
@@ -33,22 +33,45 @@ eticheta1 = tk.Label(aplicatie, text="Efectueaza împărțirile :", font=font_et
 eticheta1.pack()
 eticheta2 = tk.Label(aplicatie, text=str(a * b) + " : " + str(b) + " = ", font=font_etichete)
 eticheta2.pack()
-casuta_text = tk.Entry(aplicatie, font=font_etichete)
+casuta_text = tk.Entry(aplicatie, font=font_etichete,bg='yellow',fg='blue')
 casuta_text.pack()
+
+
+def scrie_in_casuta(x):
+    text_nou=casuta_text.get()+str(x)
+    #ms.showinfo("info",str(text_nou))
+    casuta_text.delete(0,tk.END)
+    casuta_text.insert(0,text_nou)
+
+
 eticheta_corecte = tk.Label(aplicatie, text="Corecte : 0", font=font_etichete)
 eticheta_corecte.pack()
 eticheta_incorecte = tk.Label(aplicatie, text="Greșite : 0", font=font_etichete)
 eticheta_incorecte.pack()
-cadru1=tk.Frame(aplicatie)
-cadru1.pack(fill='both',expand=True)
-for x in range(0,5):
-    buton_nr=tk.Button(cadru1,text=str(x),borderwidth=5,width=5,height=2,font=font_butoane)
-    buton_nr.pack(side='left')
+cadru1 = tk.Frame(aplicatie)
+cadru1.pack(fill='both', expand=True)
+buton_1 = tk.Button(cadru1, text='1', borderwidth=5, width=5, height=2, font=font_butoane,command=lambda:scrie_in_casuta(1),bg='lightgreen')
+buton_1.pack(side='left')
+buton_2 = tk.Button(cadru1, text='2', borderwidth=5, width=5, height=2, font=font_butoane,command=lambda:scrie_in_casuta(2),bg='lightgreen')
+buton_2.pack(side='left')
+buton_3 = tk.Button(cadru1, text='3', borderwidth=5, width=5, height=2, font=font_butoane,command=lambda:scrie_in_casuta(3),bg='lightgreen')
+buton_3.pack(side='left')
+buton_4 = tk.Button(cadru1, text='4', borderwidth=5, width=5, height=2, font=font_butoane,command=lambda:scrie_in_casuta(4),bg='lightgreen')
+buton_4.pack(side='left')
+buton_5 = tk.Button(cadru1, text='5', borderwidth=5, width=5, height=2, font=font_butoane,command=lambda:scrie_in_casuta(5),bg='lightgreen')
+buton_5.pack(side='left')
 cadru2 = tk.Frame(aplicatie)
 cadru2.pack(fill='both', expand=True)
-for x in range(5,10):
-    buton_nr=tk.Button(cadru2,text=str(x),borderwidth=5,width=5,height=2,font=font_butoane)
-    buton_nr.pack(side='left')
+buton_6 = tk.Button(cadru2, text='6', borderwidth=5, width=5, height=2, font=font_butoane,command=lambda:scrie_in_casuta(6),bg='lightgreen')
+buton_6.pack(side='left')
+buton_7 = tk.Button(cadru2, text='7', borderwidth=5, width=5, height=2, font=font_butoane,command=lambda:scrie_in_casuta(7),bg='lightgreen')
+buton_7.pack(side='left')
+buton_8 = tk.Button(cadru2, text='8', borderwidth=5, width=5, height=2, font=font_butoane,command=lambda:scrie_in_casuta(8),bg='lightgreen')
+buton_8.pack(side='left')
+buton_9 = tk.Button(cadru2, text='9', borderwidth=5, width=5, height=2, font=font_butoane,command=lambda:scrie_in_casuta(9),bg='lightgreen')
+buton_9.pack(side='left')
+buton_del = tk.Button(cadru2, text='<-', borderwidth=5, width=5, height=2, font=font_butoane,command=lambda:casuta_text.delete(0,tk.END),bg='red')
+buton_del.pack(side='left')
 
 def genereaza_numere():
     global a, b, c
