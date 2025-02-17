8e279cc8043ea8f36346e68aa54060d78fe04bfc
Coconutu <sebastian.daniel.adrian@gmail.com>
2025-02-17 07:52:31 +0200

    pictograme

--- tkinter/pictograme.py
+++ tkinter/pictograme.py
@@ -2,6 +2,15 @@ import tkinter as tk
 root=tk.Tk()
 #Caracter Unicode pentru o săgeată la stânga (←)
 sageata_stanga=u"\u2190"
-buton=tk.Button(root,text=sageata_stanga+'\n Înapoi',font="Calibri 12 bold")
-buton.pack()
+# buton1=tk.Button(root,text=sageata_stanga+'\n Înapoi',font="Calibri 12 bold")
+# buton1.pack()
+sageata_stanga=tk.PhotoImage(file="Iconite/back.png")
+sageata_dreapta=tk.PhotoImage(file="Iconite/forward.png")
+
+buton_s=tk.Button(root,image=sageata_stanga,text="Inapoi",compound='top',font="Calibri 12 bold")
+buton_s.grid(row=0,column=0,padx=20,pady=20)
+
+buton_d=tk.Button(root,image=sageata_dreapta,text="Inapoi",compound='top',font="Calibri 12 bold")
+buton_d.grid(row=0,column=1,padx=20,pady=20)
+
 root.mainloop()
