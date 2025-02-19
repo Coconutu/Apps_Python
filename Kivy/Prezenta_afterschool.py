2ebc1a35f2f828dd34e005d8a246b1a810a05706
Coconutu <sebastian.daniel.adrian@gmail.com>
2025-02-19 18:24:47 +0200

    Prezenta afterschool kivy

--- Kivy/Prezenta_afterschool.py
+++ Kivy/Prezenta_afterschool.py
@@ -2,11 +2,13 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.tabbedpanel import TabbedPanel, TabbedPanelItem
 from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+
 
 def citire_elevi():
-    with open("elevi.dat","r") as f:
-        citire=f.read()
-        citire.replace("\n", " ")
+    with open("elevi.dat", "r") as f:
+        citire = f.read()
+        # citire.replace("\n", " ")
     return citire
 
 
@@ -25,16 +27,25 @@ class MyTabbedPanel(TabbedPanel):
 
         # Creăm al treilea tab
         tab3 = TabbedPanelItem(text='Editare elevi')
-        layout=BoxLayout(orientation='vertical')
-        layout.add_widget(Label(text='Editare elevi 1'))
-        layout.add_widget(Label(text='Editare elevi 2'))
-        tab3.add_widget(layout)
+
+        layout_vertical_1 = BoxLayout(orientation='vertical', padding=10)
+        input_elev = TextInput(text='Introduceti numele elevului', multiline=False)
+        layout_vertical_1.add_widget(input_elev)
+
+        layout_vertical_2 = BoxLayout(orientation='vertical', padding=10)
+        layout_vertical_2.add_widget(input_elev)
+        layout_vertical_2.add_widget(Label(text='Editare elevi 1'))
+        layout_vertical_2.add_widget(Label(text=citire_elevi()))
 
 
-        # Adăugăm tab-urile în interfață
+        tab3.add_widget(layout_vertical_1)
+        tab3.add_widget(layout_vertical_2)
+
+
         self.add_widget(tab1)
         self.add_widget(tab2)
         self.add_widget(tab3)
+
 
 class MyApp(App):
     def build(self):
