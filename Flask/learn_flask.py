f6d1eb268efe7bf0c2d78446960d7d5afb7a2668
Coconutu <sebastian.daniel.adrian@gmail.com>
2025-03-14 08:52:51 +0200

    CAPTURE DATA FROM HTML IN A VARIABLE

--- Flask/learn_flask.py
+++ Flask/learn_flask.py
@@ -1,26 +1,15 @@
-from flask import Flask, request,render_template
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/potato')
-def route():
-    return "This is my first Flask app. Yay!"
+@app.route('/',methods=['GET','POST'])
+def rootpage():
+    name=""
+    if request.method=="POST" and 'username' in request.form:
+        name=request.form.get('username')
+
+    return render_template("index.html",name=name)
 
 
-@app.route('/')
-def hello():
-    return render_template("index.html")
-
-@app.route('/bob')
-def bob():
-    return "Yo Bob. What's hapening man!!"
-
-
-@app.route('/method', methods=['GET','POST'])
-def method():
-    if request.method == 'POST':
-        return "You're sent a POST request"
-    else:
-        return "You're probably using a GET request"
 app.run()
