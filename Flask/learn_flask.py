d85e6523d46ba7717ba86e3064a0ba94b99e8fb5
Coconutu <sebastian.daniel.adrian@gmail.com>
2025-03-15 07:19:36 +0200

    add field

--- Flask/learn_flask.py
+++ Flask/learn_flask.py
@@ -6,10 +6,12 @@ app = Flask(__name__)
 @app.route('/',methods=['GET','POST'])
 def rootpage():
     name=""
+    food=""
+
     if request.method=="POST" and 'username' in request.form:
         name=request.form.get('username')
-
-    return render_template("index.html",name=name)
+        food=request.form.get('userfood')
+    return render_template("index.html",name=name,food=food)
 
 
 app.run()
